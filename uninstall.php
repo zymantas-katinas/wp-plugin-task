04de9b50e37bdf15f7d6052deaed3abcd7f0b4c0
Zymantas Katinas <kyssaz@gmail.com>
2020-03-03 13:09:58 +0200

    add styling with activation

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Trigger on Plugin uninstall
+ * 
+ * @package ZymantasPlugin
+ */
+
+ if( ! defined( 'WP_UNINSTALL_PLUGIN' )) {
+     die;
+ }
--- zymantas-plugin.php
+++ zymantas-plugin.php
@@ -6,8 +6,42 @@
 
 /*
 Plugin Name: Zymantas Plugin
-Plugin URI: 
-Plugin Description: A plugin for wp task.
+Plugin URI: https://github.com/zymantas-katinas/wp-plugin-task
+Description: A plugin for wp task.
 Version: 1.0.0
 Author: Zymantas Katinas
+Author URI: https://www.zymantaskatinas.com
  */
+
+ if ( ! defined('ABSPATH')) {
+     die;
+ }
+
+ class ZymantasPlugin
+ {
+     function register() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue'));
+     }
+
+     function activate() {
+         flush_rewrite_rules(); 
+     }
+
+     function deactivate() {
+        flush_rewrite_rules(); 
+     }
+
+     function enqueue() {
+         wp_enqueue_style( 'style', plugins_url('/assets/newstyle.css', __FILE__) );
+     }
+ }
+
+ if( class_exists('ZymantasPlugin')){
+     $zymantasPlugin = new ZymantasPlugin();
+     $zymantasPlugin->register();
+ }
+ //activation
+ register_activation_hook( __FILE__, array ($zymantasPlugin, 'activate'));
+
+ //deactivation
+ register_deactivation_hook( __FILE__, array ($zymantasPlugin, 'deactivate'));
