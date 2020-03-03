43c9684b5b3ee030e6b17287a3697aa9cdeb83bd
Zymantas Katinas <kyssaz@gmail.com>
2020-03-03 10:57:06 +0200

    initial commit

---
+++ zymantas-plugin.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * @package ZymantasPlugin
+ */
+
+/*
+Plugin Name: Zymantas Plugin
+Plugin URI: 
+Plugin Description: A plugin for wp task.
+Version: 1.0.0
+Author: Zymantas Katinas
+ */
