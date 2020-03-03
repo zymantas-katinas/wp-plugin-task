ac8a161ac9aea0b407b4da945adea14abd4a9331
Zymantas Katinas <kyssaz@gmail.com>
2020-03-03 15:21:20 +0200

    add post triangle

--- zymantas-plugin.php
+++ zymantas-plugin.php
@@ -34,14 +34,38 @@ Author URI: https://www.zymantaskatinas.com
      function enqueue() {
          wp_enqueue_style( 'style', plugins_url('/assets/newstyle.css', __FILE__) );
      }
+
+     function addPost(){
+        add_action( 'gform_after_submission', 'create_post_example', 10, 1 );
+     }
+
  }
 
  if( class_exists('ZymantasPlugin')){
      $zymantasPlugin = new ZymantasPlugin();
      $zymantasPlugin->register();
+     $zymantasPlugin->addPost();
  }
  //activation
  register_activation_hook( __FILE__, array ($zymantasPlugin, 'activate'));
 
  //deactivation
  register_deactivation_hook( __FILE__, array ($zymantasPlugin, 'deactivate'));
+
+
+ function create_post_example($entry) {
+    $post_data = array(
+        'post_title' => rgar( $entry, 'date_created' ). ' pusperimetris - ' .rgar($entry, '6'),
+        'post_content' => 
+            'Pirma kraštinė - '.rgar($entry, '3').
+            '<br>Antra kraštinė - '.rgar($entry, '4').
+            '<br>Trečia kraštinė - '.rgar($entry, '5'),
+        'post_status' => 'publish',
+        'post_type' => 'post'
+    );
+
+    wp_insert_post( $post_data );
+}
+
+
+
